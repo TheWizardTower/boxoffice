7eacce485e5daf182c1e57c354a42565913a4aec
Adam McCullough <merlinfmct87@gmail.com>
2017-09-17 02:35:36 -0700

    This compiles, but is fundamentally broken. We need monad transformers to fix.

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,33 @@ module Lib
     ( someFunc
     ) where
 
+import Control.Monad.Trans.State
+import Data.IORef
+import Data.Time.Clock
+
+staticVar :: IO (IORef ([UTCTime]))
+staticVar = newIORef []
+
+resetCount :: IO ()
+resetCount = do
+  ((flip modifyIORef') returnEmptyList) <$> staticVar
+  return ()
+
+returnEmptyList :: [UTCTime] -> [UTCTime]
+returnEmptyList _ = []
+
+addCount :: IO ()
+addCount = do
+  currentTime <- getCurrentTime
+  ((flip modifyIORef') (addToList currentTime)) <$> staticVar
+  return ()
+
+addToList :: a -> [a] -> [a]
+addToList time list = time : list
+
+showList = do
+  list <- fmap readIORef staticVar
+  fmap (putStrLn . show) list
+
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
